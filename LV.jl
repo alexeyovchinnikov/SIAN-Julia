f08ca313ed96e81df56ed20cd585074ef2dc20bf
alexeyovchinnikov <56935058+alexeyovchinnikov@users.noreply.github.com>
2020-10-17 11:48:33 -0400

    Add files via upload

--- LV.jl
+++ LV.jl
@@ -2,17 +2,19 @@ println("Setting up the problem")
 
 include("IdentifiabilityODE.jl")
 
+using BenchmarkTools
+
 prob=0.99
-R, (x1,x2,y1,u,a,b,c,d) = Oscar.PolynomialRing(Oscar.QQ, ["x1","x2","y1","u","a","b","c","d"], ordering=:degrevlex)
+R, (x1,x2,y1,u,v,a,b,c,d) = Nemo.PolynomialRing(Nemo.QQ, ["x1","x2","y1","u","v","a","b","c","d"], ordering=:degrevlex)
 
 sigma_x = [
             [x1, a*x1 - b*x1*x2],
             [x2, -c*x2 + d*x1*x2],
           ]
 sigma_y = [
-            [y1, x1+u]
+            [y1, x1+u+v]
           ]
 
-IdentifiabilityODE(sigma_x, sigma_y,  [u], [a,b,c,d], [x1], prob)
+IdentifiabilityODE(sigma_x, sigma_y,  [u,v], GetParameters(sigma_x, sigma_y, [u,v]), prob)
 
 
