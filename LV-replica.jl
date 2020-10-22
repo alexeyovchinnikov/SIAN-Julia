475d3f81a4e26fa39e4669db0ba7fa3499c51175
alexeyovchinnikov <56935058+alexeyovchinnikov@users.noreply.github.com>
2020-10-22 10:17:07 -0400

    Add files via upload

--- LV-replica.jl
+++ LV-replica.jl
@@ -1,24 +1,21 @@
-println("Setting up the problem")
-
 include("IdentifiabilityODE.jl")
 
-using BenchmarkTools
+println("Setting up the problem")
 
-prob=0.99
-R, (x1,x2,y1,u,v,a,b,c,d) = Nemo.PolynomialRing(Nemo.QQ, ["x1","x2","y1","u","v","a","b","c","d"], ordering=:degrevlex)
+R, (x1, x2, y1, u, v, a, b, c, d) = Nemo.PolynomialRing(Nemo.QQ, ["x1", "x2", "y1", "u", "v", "a", "b", "c", "d"])
 
 sigma_x = [
-            [x1, a*x1 - b*x1*x2],
-            [x2, -c*x2 + d*x1*x2],
+            [x1, a * x1 - b * x1 * x2],
+            [x2, -c * x2 + d * x1 * x2],
           ]
 sigma_y = [
-            [y1, x1+u+v]
+            [y1, x1 + u + v]
           ]
 
 r=3
 
 sigma_x_r, sigma_y_r, u_vars_r = generate_replica(sigma_x, sigma_y, [u,v], r)
 
-identifiability_ode(sigma_x_r, sigma_y_r, u_vars_r, get_parameters(sigma_x_r, sigma_y_r, u_vars_r), prob)
+identifiability_ode(sigma_x_r, sigma_y_r, u_vars_r, get_parameters(sigma_x_r, sigma_y_r, u_vars_r); p = 0.99, p_mod = 2^29 - 3, nthrds = 64)
 
 
