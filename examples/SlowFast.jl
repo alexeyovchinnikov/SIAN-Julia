5fc40cfadf21d502384334b6cf1ad210145b1857
Ilia Ilmer <iilmer@icloud.com>
2020-12-24 17:03:40 -0500

    fix nthrds in readme and in one commented code

--- examples/SlowFast.jl
+++ examples/SlowFast.jl
@@ -20,7 +20,7 @@ identifiability_ode(ode, get_parameters(ode); p=0.99, p_mod=0, nthrds=1)
 # generate_replica(ode, 2) generates a system consiting of two copies of sigma
 # with the same parameters in ODEs but different inputs and initial conditions
 
-r = 2
+# r = 2
 
 # identifiability_ode(generate_replica(ode, r), get_parameters(ode; initial_conditions = false); p = 0.99, p_mod = 0, nthrds = 64)
 
