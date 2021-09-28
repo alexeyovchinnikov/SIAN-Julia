637ff7d295f286d5e54d89b5c60e724ab0d333fd
Ilia Ilmer <i.ilmer@icloud.com>
2021-09-28 18:02:13 -0400

    make dict{any,any} in test

--- test/mtk_compat.jl
+++ test/mtk_compat.jl
@@ -6,7 +6,7 @@
     eqs = [D(x0) ~ -(a01 + a21) * x0 + a12 * x1, D(x1) ~ a21 * x0 - a12 * x1, y1 ~ x0]
 
     de = ODESystem(eqs, t, name=:Test)
-    correct = Dict(
+    correct = Dict{Any,Any}(
         "nonidentifiable" => [a12, a21, substitute(x1, t=>0), a01], 
         "locally_not_globally" => Num[], 
         "globally" => [substitute(x0, t=>0)]
