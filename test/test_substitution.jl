14fb8070dd8610e797671648ed51141ed39c09b1
Ilia <i.ilmer@icloud.com>
2022-02-14 17:33:55 -0500

    make_substitution: import in tests, iterate over sets,

--- test/test_substitution.jl
+++ test/test_substitution.jl
@@ -1,14 +1,17 @@
 @testset "Testing weight substitution" begin
+    using StructuralIdentifiability: make_substitution
     R, (x, y, z, a, b) = SIAN.Nemo.PolynomialRing(SIAN.Nemo.QQ, [:x, :y, :z, :a, :b])
 
     Et_hat = [x + y, a * x + y * z, b - x^2 + y^3 * z]
-    Et_hat_sub = [x^3 + y, a^2 * x^2 + y * z, b^2 - x^6 + y^3 * z]
+    Et_hat_sub = [x^3 + y, a^2 * x^3 + y * z, b^2 - x^6 + y^3 * z]
 
     weights = Dict(a => 2, b => 2, x => 3)
 
     for i in 1:length(Et_hat)
-        for _var in SIAN.Nemo.vars(Et_hat[i])
-            Et_hat[i] = SIAN.StrucrturalIdentifiability.make_substitution(Et_hat[i], _var, _var^get(weights, _var, 1), parent(_var)(1))
+        for _var in Set(SIAN.Nemo.vars(Et_hat[i]))
+            println(_var, Et_hat[i])
+            Et_hat[i] = make_substitution(Et_hat[i], _var, _var^get(weights, _var, 1), parent(_var)(1))
+            println(_var, Et_hat[i])
         end
     end
 
