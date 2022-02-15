f92852b64826fd189e676b494c2d575ee91d8dd0
Ilia <i.ilmer@icloud.com>
2022-02-14 22:02:46 -0500

    make_substitution error

--- test/test_substitution.jl
+++ test/test_substitution.jl
@@ -1,5 +1,5 @@
 @testset "Testing weight substitution" begin
-    using StructuralIdentifiability: make_substitution
+
     @info "Case 1"
     R, (x, y, z, a, b) = SIAN.Nemo.PolynomialRing(SIAN.Nemo.QQ, [:x, :y, :z, :a, :b])
     Et_hat = [x + y, a * x + y * z, b - x^2 + y^3 * z]
@@ -7,9 +7,7 @@
     weights = Dict(a => 2, b => 2, x => 3)
     for i in 1:length(Et_hat)
         for _var in Set(SIAN.Nemo.vars(Et_hat[i]))
-            println(_var, Et_hat[i])
-            Et_hat[i] = make_substitution(Et_hat[i], _var, _var^get(weights, _var, 1), parent(_var)(1))
-            println(_var, Et_hat[i])
+            Et_hat[i] = SIAN.make_substitution(Et_hat[i], _var, _var^get(weights, _var, 1), parent(_var)(1))
         end
     end
     @test all(Et_hat_sub[i] == Et_hat[i] for i ∈ 1:length(Et_hat))
@@ -21,20 +19,19 @@
     weights = Dict(x => 1, a => 2, z => 5)
     for i in 1:length(Et_hat)
         for _var in Set(SIAN.Nemo.vars(Et_hat[i]))
-            Et_hat[i] = make_substitution(Et_hat[i], _var, _var^get(weights, _var, 1), parent(_var)(1))
+            Et_hat[i] = SIAN.make_substitution(Et_hat[i], _var, _var^get(weights, _var, 1), parent(_var)(1))
         end
     end
     @test all(Et_hat_sub[i] == Et_hat[i] for i ∈ 1:length(Et_hat))
 
     @info "Case 3"
-
     R, (x, y, z) = SIAN.Nemo.PolynomialRing(SIAN.Nemo.QQ, [:x, :y, :z, :a, :b])
     Et_hat = [x - y^3, x^3 + y, z - y^2, x + z^2]
     Et_hat_sub = [x - y^3, x + y, z - y^2, x + z^2]
     weights = Dict(x => 2, y => 2, z => 3)
     for i in 1:length(Et_hat)
         for _var in Set(SIAN.Nemo.vars(Et_hat[i]))
-            Et_hat[i] = make_substitution(Et_hat[i], _var, _var^get(weights, _var, 1), parent(_var)(1))
+            Et_hat[i] = SIAN.make_substitution(Et_hat[i], _var, _var^get(weights, _var, 1), parent(_var)(1))
         end
     end
     @test all(Et_hat_sub[i] == Et_hat[i] for i ∈ 1:length(Et_hat))
