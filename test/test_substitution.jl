affd1b8dc7d3cd6f3e544a16ebf1bca99e132fc5
Ilia <i.ilmer@icloud.com>
2022-02-15 07:43:56 -0500

    macro error: tests were failing

--- test/test_substitution.jl
+++ test/test_substitution.jl
@@ -15,7 +15,7 @@
     @info "Case 2"
     R, (x, y, z, a, b, c) = SIAN.Nemo.PolynomialRing(SIAN.Nemo.QQ, [:x, :y, :z, :a, :b, :c])
     Et_hat = [x * y - a, 3 * x + y, 2 * x * y * z * a - z, x * y^2 + y^3 * z^4 - c^2 * b * x^2]
-    Et_hat_sub = [x * y - a^2, 3 * x + y, 2 * x * y * z * a - z^4, x * y^2 + y^3 * z^20 - c^2 * b * x^2]
+    Et_hat_sub = [x * y - a^2, 3 * x + y, 2 * x * y * z^5 * a^2 - z^5, x * y^2 + y^3 * z^20 - c^2 * b * x^2]
     weights = Dict(x => 1, a => 2, z => 5)
     for i in 1:length(Et_hat)
         for _var in Set(SIAN.Nemo.vars(Et_hat[i]))
@@ -27,7 +27,7 @@
     @info "Case 3"
     R, (x, y, z) = SIAN.Nemo.PolynomialRing(SIAN.Nemo.QQ, [:x, :y, :z, :a, :b])
     Et_hat = [x - y^3, x^3 + y, z - y^2, x + z^2]
-    Et_hat_sub = [x - y^3, x + y, z - y^2, x + z^2]
+    Et_hat_sub = [x^2 - y^6, x^6 + y^2, z^3 - y^4, x^2 + z^6]
     weights = Dict(x => 2, y => 2, z => 3)
     for i in 1:length(Et_hat)
         for _var in Set(SIAN.Nemo.vars(Et_hat[i]))
