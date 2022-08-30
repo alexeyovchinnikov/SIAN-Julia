8d5018f56510d890dd61910b5fa07318cc952bc0
Ilia <i.ilmer@icloud.com>
2022-08-30 14:09:16 -0400

    measured quantitites
    Signed-off-by: Ilia <i.ilmer@icloud.com>

--- test/mtk_compat.jl
+++ test/mtk_compat.jl
@@ -154,7 +154,7 @@
 
     for case in test_cases
         ode = case[1]
-        result = identifiability_ode(ode; weighted_ordering=true, known_states=[])
+        result = identifiability_ode(ode; measured_quantities=measured_quantities, weighted_ordering=true, known_states=[])
         result_no_weights = identifiability_ode(ode; measured_quantities=case[end], weighted_ordering=false, known_states=[])
 
         @test case[2] == result
